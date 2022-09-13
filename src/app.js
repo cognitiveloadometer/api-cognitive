eb32c36f2f5780374b0da35ad63c4589428043b1
Tales <jobbtales@gmail.com>
2022-09-12 23:13:19 -0300

    test

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import { startDatabase } from "./database/index.js";
 import usersRoutes from "./routes/users.routes.js"
 import teamsRoutes from "./routes/teams.routes.js"
 import cognitiveRoutes from "./routes/cognitive.routes.js"
+import cors from "cors"
 
 const app = express()
 const port = 3030
 
 app.use(express.json())
 
-app.use("/users", usersRoutes)
+app.use("/users", cors(), usersRoutes)
 app.use("/teams", teamsRoutes)
 app.use("/cognitiveloadmeter", cognitiveRoutes)
 
